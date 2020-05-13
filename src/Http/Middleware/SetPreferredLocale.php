1b536d918cf456cee4177af2404b9be5f0680b7a
Sander de Vos <sander@tutanota.de>
2020-05-13 23:32:39 +0200

    Set preferred locale middleware

---
+++ src/Http/Middleware/SetPreferredLocale.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Soved\Laravel\Helpers\Http\Middleware;
+
+use App;
+use Closure;
+
+class SetPreferredLocale
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return mixed
+     */
+    public function handle(
+        $request,
+        Closure $next
+    ) {
+        $availableLocales = [
+            config('app.locale'),
+            config('app.fallback_locale'),
+        ];
+
+        $locale = $request->getPreferredLanguage($availableLocales);
+
+        App::setLocale($locale);
+
+        return $next($request);
+    }
+}
---
+++ tests/Feature/SetPreferredLocaleMiddlewareTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Unit;
+
+use App;
+use Tests\TestCase;
+use Illuminate\Support\Facades\Route;
+use Soved\Laravel\Helpers\Http\Middleware\SetPreferredLocale;
+
+class SetPreferredLocaleMiddlewareTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Route::get('middleware-test', fn () => 'test')->middleware(SetPreferredLocale::class);
+    }
+
+    public function testDefaultLocale(): void
+    {
+        $defaultLocale = config('app.locale');
+
+        App::setLocale('ru'); // ISO 639-1 language code for Russian
+
+        $response = $this->get('middleware-test');
+
+        $response->assertOk();
+
+        $this->assertEquals($defaultLocale, App::getLocale());
+    }
+
+    public function testPreferredLocale(): void
+    {
+        $defaultLocale = config('app.locale');
+        $preferredLocale = 'nl'; // ISO 639-1 language code for Dutch
+
+        config(['app.fallback_locale' => $preferredLocale]);
+
+        $this->assertEquals($defaultLocale, App::getLocale());
+
+        $response = $this
+            ->withHeaders([
+                'Accept-Language' => 'nl,en-US;q=0.9,en-GB;q=0.8,en;q=0.7',
+            ])
+            ->get('middleware-test');
+
+        $response->assertOk();
+
+        $this->assertEquals($preferredLocale, App::getLocale());
+    }
+}
